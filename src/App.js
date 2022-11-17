b7eeb046cdaedec4e6abb7c46f878783a642e0cc
habil99 <abiyevhabil3@gmail.com>
2022-11-17 17:22:53 +0400

    Init d3 graph

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import D3Graph from "./main.js";
+import { useEffect, useState } from "react";
+
 
 function App() {
+  const [position, setPosition] = useState({ x: window.innerWidth / 2, y: window.innerHeight / 2 });
+
+  useEffect(() => {
+    const d3Graph = new D3Graph();
+    const radius = 45;
+    const nodes = [
+      {
+        id: 1,
+        clipId: 'clip1',
+        name: 'Node 1',
+        x: window.innerWidth / 2 - 250,
+        y: window.innerHeight / 2 - 350,
+        r: radius,
+        color: 'red',
+        image: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHByb2ZpbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+
+      },
+      {
+        id: 2,
+        clipId: 'clip2',
+        name: 'Node 2',
+        x: window.innerWidth / 2 + radius,
+        y: window.innerHeight / 2 - radius,
+        r: radius,
+        color: 'green',
+        image: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHByb2ZpbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+      },
+      {
+        id: 3,
+        clipId: 'clip3',
+        name: 'Node 3',
+        x: window.innerWidth / 2 + 150,
+        y: window.innerHeight / 2 + 250,
+        r: radius,
+        color: 'green',
+        image: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHByb2ZpbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+      },
+    ]
+
+    nodes.forEach(node => {
+      d3Graph.createCircleWithImage(node.x, node.y, node.r, node.image, node.clipId);
+    })
+
+    d3Graph.connectAllNodes(nodes)
+    // d3Graph.initZoom();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <svg></svg>
   );
 }
 
